d2db3b40f9387479658359a471f7e2fed5b41f81
Erik Hornberger <hornbergererik@gmail.com>
2018-07-14 00:59:28 +0900

    Added motor control

--- src/Motor.py
+++ src/Motor.py
@@ -1,26 +1,32 @@
 from utils import make_logger
+from RPi import GPIO
+
 
 class Motor(object):
 
     def __init__(self, plus_pin, minus_pin):
+        GPIO.setup([plus_pin, minus_pin], GPIO.OUT)
         self.logger = make_logger(__name__)
         self.plus_pin = plus_pin
         self.minus_pin = minus_pin
+        self.stop()
 
-    def run(self):
-        try:
-            self.logger.info('Running motor!')
-            pass
-        except Exception as error:
-            self.logger.error(error, exc_info=True)
-    
+    def start(self):
+        self.logger.info('Starting motor!')
+        GPIO.output(self.plus_pin, GPIO.HIGH)
+
     def stop(self):
-        try:
-            self.logger.info('Stopping motor!')
-        except Exception as error:
-            self.logger.error(error, exc_info=True)
+        self.logger.info('Stopping motor!')
+        GPIO.output(self.plus_pin, GPIO.LOW)
+        GPIO.output(self.minus_pin, GPIO.LOW)
+
 
 if __name__ == "__main__":
-    motor = Motor(1, 2)
-    motor.run()
+    from time import sleep
+    GPIO.setmode(GPIO.BOARD)
+    motor = Motor(37, 35)
+    motor.start()
+    sleep(2.0)
     motor.stop()
+
+    GPIO.cleanup()
--- src/Senseekan.py
+++ src/Senseekan.py
@@ -16,51 +16,86 @@ class Senseekan(WiimoteDelegate):
         self.logger.info('Attempting to connect wiimote...')
         self.wiimote.search_and_connect()
 
+    def start_going_forward(self):
+        self.left_motor.start()
+        self.right_motor.start()
+
+    def start_turning_right(self):
+        self.left_motor.start()
+        self.right_motor.stop()
+
+    def start_turning_left(self):
+        self.right_motor.start()
+        self.left_motor.stop()
+
+    def stop(self):
+        self.right_motor.stop()
+        self.left_motor.stop()
+
     # Wiimote Delegate Methods
+
     def wiimote_did_connect(self, wiimote):
         self.logger.info('Connected to wiimote!')
 
     def wiimote_disconnected(self, wiimote):
         self.logger.info('Wiimote disconnected!')
+        self.stop()
         self.connect()
 
-    def wiimote_pressed_A(self, wiimote):
-        self.logger.debug('Pressed A')
-
-    def wiimote_released_A(self, wiimote):
-        self.logger.debug('Released A')
-
-    def wiimote_pressed_B(self, wiimote):
-        self.logger.debug('Pressed B')
-
-    def wiimote_released_B(self, wiimote):
-        self.logger.debug('Released B')
-
     def wiimote_pressed_left(self, wiimote):
-        self.logger.debug('Pressed Left')
+        self.logger.debug('Turning Left')
+        self.start_turning_left()
 
     def wiimote_released_left(self, wiimote):
-        self.logger.debug('Released Left')
+        self.logger.debug('Stopping Left Turn')
+        self.stop()
 
     def wiimote_pressed_right(self, wiimote):
-        self.logger.debug('Pressed Right')
+        self.logger.debug('Turning Right')
+        self.start_turning_right()
 
     def wiimote_released_right(self, wiimote):
-        self.logger.debug('Released Right')
+        self.logger.debug('Stopping Right Turn')
+        self.stop()
 
     def wiimote_pressed_up(self, wiimote):
-        self.logger.debug('Pressed Up')
+        self.logger.debug('Going Forward')
+        self.start_going_forward()
 
     def wiimote_released_up(self, wiimote):
-        self.logger.debug('Released Up')
+        self.logger.debug('Stopping Going Foward')
+        self.stop()
 
     def wiimote_pressed_down(self, wiimote):
-        self.logger.debug('Pressed Dowear')
+        pass
 
     def wiimote_released_down(self, wiimote):
-        self.logger.debug('Released Down')
+        pass
+
+    def wiimote_pressed_A(self, wiimote):
+        pass
+
+    def wiimote_released_A(self, wiimote):
+        pass
+
+    def wiimote_pressed_B(self, wiimote):
+        pass
+
+    def wiimote_released_B(self, wiimote):
+        pass
 
 
 if __name__ == '__main__':
-    seekan = Senseekan(Motor(37, 35), Motor(33, 31))
-    seekan.connect()
+    from RPi import GPIO
+    GPIO.setmode(GPIO.BOARD)
+
+    left = Motor(37, 35)
+    right = Motor(31, 33)
+    seekan = Senseekan(left, right)
+
+    try:
+        seekan.connect()
+    except KeyboardInterrupt:
+        print("Cleaning up...")
+        GPIO.cleanup()
+        print("Done")
