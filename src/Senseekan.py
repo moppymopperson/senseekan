c5b41531253d645e85514352350e1d6091a2630a
Erik Hornberger <hornbergererik@gmail.com>
2018-07-13 22:52:39 +0900

    Handle disconnect gracefully

--- src/Senseekan.py
+++ src/Senseekan.py
@@ -3,6 +3,7 @@ from utils import make_logger
 from Motor import Motor
 from Wiimote import Wiimote, WiimoteDelegate
 
+
 class Senseekan(WiimoteDelegate):
 
     def __init__(self, left_motor, right_motor):
@@ -14,17 +15,18 @@ class Senseekan(WiimoteDelegate):
     def connect(self):
         self.logger.info('Attempting to connect wiimote...')
         self.wiimote.search_and_connect()
-        
+
     # Wiimote Delegate Methods
     def wiimote_did_connect(self, wiimote):
         self.logger.info('Connected to wiimote!')
 
     def wiimote_disconnected(self, wiimote):
         self.logger.info('Wiimote disconnected!')
+        self.connect()
 
     def wiimote_pressed_A(self, wiimote):
         self.logger.debug('Pressed A')
-    
+
     def wiimote_released_A(self, wiimote):
         self.logger.debug('Released A')
 
@@ -33,7 +35,7 @@ class Senseekan(WiimoteDelegate):
 
     def wiimote_released_B(self, wiimote):
         self.logger.debug('Released B')
-    
+
     def wiimote_pressed_left(self, wiimote):
         self.logger.debug('Pressed Left')
 
@@ -42,7 +44,7 @@ class Senseekan(WiimoteDelegate):
 
     def wiimote_pressed_right(self, wiimote):
         self.logger.debug('Pressed Right')
-    
+
     def wiimote_released_right(self, wiimote):
         self.logger.debug('Released Right')
 
@@ -57,10 +59,8 @@ class Senseekan(WiimoteDelegate):
 
     def wiimote_released_down(self, wiimote):
         self.logger.debug('Released Down')
-        
+
 
 if __name__ == '__main__':
-    left_motor = Motor(37, 35)
-    right_motor = Motor(33, 31)
-    seekan = Senseekan(left_motor, right_motor)
+    seekan = Senseekan(Motor(37, 35), Motor(33, 31))
     seekan.connect()
--- src/Wiimote.py
+++ src/Wiimote.py
@@ -1,8 +1,12 @@
 from abc import ABCMeta, abstractmethod
-from utils import make_logger
 from threading import Thread
 from time import sleep
+import re
+import subprocess
 import cwiid
+
+from utils import make_logger
+
 
 class WiimoteDelegate:
     __metaclass__ = ABCMeta
@@ -14,7 +18,7 @@ class WiimoteDelegate:
     @abstractmethod
     def wiimote_disconnected(self, wiimote):
         pass
-    
+
     @abstractmethod
     def wiimote_pressed_A(self, wiimote):
         pass
@@ -63,15 +67,22 @@ class WiimoteDelegate:
     def wiimote_released_down(self, wiimote):
         pass
 
+
 class Wiimote(object):
 
     def __init__(self, delegate=None):
+        # Public
         self.logger = make_logger(__name__)
         self.logger.info('Created new Wiimote!')
         self.delegate = delegate
-        self._is_connected = False
-        self.worker_thread = Thread(target=self._loop)
         self.check_interval = 0.1
+        self.watchdog_timeout = 1.0
+
+        # Private
+        self._is_connected = False
+        self._worker_thread = None
+        self._watchdog_thread = None
+        self._should_terminate = False
         self._remote = None
         self._left_pressed = False
         self._right_pressed = False
@@ -89,51 +100,109 @@ class Wiimote(object):
         try:
             self._remote = cwiid.Wiimote()
             self._remote.rpt_mode = cwiid.RPT_BTN
+            self._set_disconnect_timeout()
             self.rumble(0.3, 3)
 
             self.logger.info('Connected to wiimote!')
             self._is_connected = True
             if self.delegate is not None:
                 self.delegate.wiimote_did_connect(self)
-            self.worker_thread.start()
+
+            self._worker_thread = Thread(target=self._loop)
+            self._worker_thread.start()
+
+            self._watchdog_thread = Thread(target=self._watchdog_loop)
+            self._watchdog_thread.start()
+
         except RuntimeError:
-            self.logger.warn('Failed to connect to remote. Attempting again.')
+            self.logger.warning(
+                'Failed to connect to remote. Attempting again.')
             self.search_and_connect()
 
     def rumble(self, duration=1, times=1):
         if self._remote is None:
-            self.logger.warn('Cannot rumble in disconnected state!')
+            self.logger.warning('Cannot rumble in disconnected state!')
             return
-        
+
         for _ in range(times):
             self._remote.rumble = 1
             sleep(duration)
             self._remote.rumble = 0
             sleep(duration)
 
-    def _loop(self):
-        self.logger.debug('Beginning loop')
-
-        while True:
-            sleep(self.check_interval)
-            buttons = self._remote.state['buttons']
-            self._handle_presses(buttons)
-            if self._check_exit_condition(buttons) == True:
-                break
-
+    def disconnect(self):
         self.logger.info('User disconnected wiimote!')
+        if self._remote is not None:
+            self.rumble()
+            self._remote.close()
+        self._should_terminate = True
+        self._watchdog_thread.join()
         self._reset_state()
-        self.rumble()
         if self.delegate is not None:
             self.delegate.wiimote_disconnected(self)
 
+    def _loop(self):
+        self.logger.debug('Beginning loop')
 
-    def _check_exit_condition(self, buttons):
+        while not self._should_terminate:
+            sleep(self.check_interval)
+            self._handle_presses()
+            if self._check_exit_condition():
+                break
+
+        self.disconnect()
+
+    def _check_exit_condition(self):
+        # If the remote has been disconnected, we'll count that is an exit
+        if self._remote is None:
+            return True
+
         # Check if + and - are pressed together
+        buttons = self._remote.state['buttons']
         return buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0
 
-    def _handle_presses(self, buttons):
-        if (buttons & cwiid.BTN_A):
+    def _watchdog_loop(self):
+        # Check to see if the remote is still connected and force the thread
+        # to exit if there is an issue
+        self.logger.info('Watchdog loop starting up')
+        while not self._should_terminate:
+            try:
+                self.logger.debug('request_status')
+                self._remote.request_status()
+                sleep(self.watchdog_timeout)
+            except ValueError:
+                self.logger.warning('Wiimote was closed when watchdog expired')
+            except RuntimeError:
+                self.logger.error('Wiimote disconnected!')
+                self._should_terminate = True
+                self._remote = None
+
+    def _set_disconnect_timeout(self):
+        # See https://github.com/abstrakraft/cwiid/issues/23 for details
+        connected_devices = subprocess.check_output(("hcitool", "con"))
+
+        # extract bluetooth MAC addresses
+        addresses = re.findall(
+            r"(([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))", connected_devices)
+        for i in addresses:
+            name = subprocess.check_output(("hcitool", "name", i[0]))
+            # identify Wiimotes. You may need to change this if looking for
+            # balance boards we are also assuming that you only have one
+            # Wiimote attached at one time, or that we want to set them all to
+            # have the same timeout
+            if name.strip() == "Nintendo RVL-CNT-01":
+                self.logger.info('Setting timeout...')
+                subprocess.call(
+                    ("sudo", "hcitool", "lst", i[0],
+                     str(1000 * self.watchdog_timeout*16/10)))
+
+    def _handle_presses(self):
+        if self._remote is None:
+            self.logger.warning('Checked presses for discconected wiimote')
+            return
+        buttons = self._remote.state['buttons']
+
+        if buttons & cwiid.BTN_A:
             self.logger.debug('Pressed A')
             if not self._A_pressed:
                 self._A_pressed = True
@@ -145,7 +214,7 @@ class Wiimote(object):
             if self.delegate is not None:
                 self.delegate.wiimote_released_A(self)
 
-        if (buttons & cwiid.BTN_B):
+        if buttons & cwiid.BTN_B:
             self.logger.debug('Pressed B')
             if not self._B_pressed:
                 self._B_pressed = True
@@ -157,7 +226,7 @@ class Wiimote(object):
             if self.delegate is not None:
                 self.delegate.wiimote_released_B(self)
 
-        if (buttons & cwiid.BTN_UP):
+        if buttons & cwiid.BTN_UP:
             self.logger.debug('Pressed Up')
             if self._up_pressed:
                 self._up_pressed = True
@@ -169,7 +238,7 @@ class Wiimote(object):
             if self.delegate is not None:
                 self.delegate.wiimote_released_up(self)
 
-        if (buttons & cwiid.BTN_DOWN):
+        if buttons & cwiid.BTN_DOWN:
             self.logger.debug('Pressed Down')
             if not self._down_pressed:
                 self._down_pressed = True
@@ -181,7 +250,7 @@ class Wiimote(object):
             if self.delegate is not None:
                 self.delegate.wiimote_released_down(self)
 
-        if (buttons & cwiid.BTN_LEFT):
+        if buttons & cwiid.BTN_LEFT:
             self.logger.debug('Pressed Left')
             if not self._left_pressed:
                 self._left_pressed = True
@@ -192,8 +261,8 @@ class Wiimote(object):
             self._left_pressed = False
             if self.delegate is not None:
                 self.delegate.wiimote_released_left(self)
-        
-        if (buttons & cwiid.BTN_RIGHT):
+
+        if buttons & cwiid.BTN_RIGHT:
             self.logger.debug('Pressed Right')
             if not self._right_pressed:
                 self._right_pressed = True
@@ -207,6 +276,8 @@ class Wiimote(object):
 
     def _reset_state(self):
         self.logger.info('Resetting wiimote to original state')
+        self._is_connected = False
+        self._should_terminate = False
 
         if self._A_pressed:
             self._A_pressed = False
